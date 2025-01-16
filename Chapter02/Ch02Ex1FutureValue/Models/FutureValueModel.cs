33b40a81f0bc7b5ae1c490392c5cdb59940a257b
FitoFire <68967952+fitofire@users.noreply.github.com>
2025-01-15 21:43:49 -0800

    Added data validation to the Future Value app

--- Chapter02/Ch02Ex1FutureValue/Models/FutureValueModel.cs
+++ Chapter02/Ch02Ex1FutureValue/Models/FutureValueModel.cs
@@ -1,16 +1,26 @@
-﻿namespace Ch02Ex1FutureValue.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ch02Ex1FutureValue.Models
 {
     public class FutureValueModel
     {
-        public decimal MonthlyInvestment { get; set; }
-        public decimal YearlyInterestRate { get; set; }
-        public int Years { get; set; }
+        [Required(ErrorMessage = "Please enter a monthly investment.")]
+        [Range(1,500, ErrorMessage = "Monthly investment amount must be between 1-500.")]
+        public decimal? MonthlyInvestment { get; set; }
 
-        public decimal CalculateFutureValue()
+        [Required(ErrorMessage = "Please enter a yearly interest rate.")]
+        [Range(0.1, 10.0, ErrorMessage = "Yearly interest rate must be between 0.1 - 10.0.")]
+        public decimal? YearlyInterestRate { get; set; }
+
+        [Required(ErrorMessage = "Please enter a number of years.")]
+        [Range(1, 50, ErrorMessage = "Number of years must be between 1-50.")]
+        public int? Years { get; set; }
+
+        public decimal? CalculateFutureValue()
         {
-            int months = Years * 12;
-            decimal monthlyInterestRate = YearlyInterestRate / 12 / 100;
-            decimal futureValue = 0;
+            int? months = Years * 12;
+            decimal? monthlyInterestRate = YearlyInterestRate / 12 / 100;
+            decimal? futureValue = 0;
 
             for (int i = 0; i < months; i++)
             {
