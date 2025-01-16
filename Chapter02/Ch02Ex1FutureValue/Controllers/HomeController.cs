81fe81e2ddbe6ab587d37f30f926d1accc74cdaa
FitoFire <68967952+fitofire@users.noreply.github.com>
2025-01-15 20:56:51 -0800

    Added HomeController.cs

---
+++ Chapter02/Ch02Ex1FutureValue/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ch02Ex1FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Morty";
+            ViewBag.Fv = 99999.99;
+            return View();
+        }
+    }
+}
