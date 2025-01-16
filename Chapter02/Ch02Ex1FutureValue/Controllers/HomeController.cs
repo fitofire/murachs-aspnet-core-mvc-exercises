6e39733d63542aca5747fa154139555c08a19af8
FitoFire <68967952+fitofire@users.noreply.github.com>
2025-01-15 21:26:24 -0800

    Updated /Controllers/HomeController.cs file

--- Chapter02/Ch02Ex1FutureValue/Controllers/HomeController.cs
+++ Chapter02/Ch02Ex1FutureValue/Controllers/HomeController.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Ch02Ex1FutureValue.Models;
 
 namespace Ch02Ex1FutureValue.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.Name = "Morty";
-            ViewBag.Fv = 99999.99;
+            ViewBag.Fv = 0;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
         }
     }
 }
