f13dc6c311c760eb2a0ecb7cf35661c0b5843818
FitoFire <68967952+fitofire@users.noreply.github.com>
2025-01-23 19:56:02 -0800

    Modified Program.cs GetConnectionString() to 'DefaultConnection'

--- Chapter04/Ch04Ex1MovieList/Program.cs
+++ Chapter04/Ch04Ex1MovieList/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllersWithViews();
 
 // Add Entity Framework Core enabling Dependency Injection for DbContext objects.
 builder.Services.AddDbContext<MovieContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("MovieContext")));
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
