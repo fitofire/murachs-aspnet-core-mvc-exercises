52abce633ff3bc071bebca6f4ad1b5b9e17c4d44
FitoFire <68967952+fitofire@users.noreply.github.com>
2025-01-23 19:09:07 -0800

    Added a Movie.cs to /Models folder and added code

---
+++ Chapter04/Ch04Ex1MovieList/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿// required to add data annoatations
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ch04Ex1MovieList.Models
+{
+    public class Movie
+    {
+        // Entity Framework Core will configure the database to generate this value
+        public int MovieId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1889, 2999, ErrorMessage = "Year must be between 1889 - 2999.")]
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a rating.")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 - 5.")]
+        public int? Rating { get; set; }
+    }
+}
