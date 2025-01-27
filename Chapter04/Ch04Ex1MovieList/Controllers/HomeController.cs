9b6ca60642b53e3ee58ef30330817df9d43474ee
FitoFire <68967952+fitofire@users.noreply.github.com>
2025-01-27 12:12:46 -0800

    Modified HomeController.cs

--- Chapter04/Ch04Ex1MovieList/Controllers/HomeController.cs
+++ Chapter04/Ch04Ex1MovieList/Controllers/HomeController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Ch04Ex1MovieList.Models;
 
 namespace Ch04Ex1MovieList.Controllers
 {
     public class HomeController : Controller
     {
+        private MovieContext context {  get; set; }
+        public HomeController(MovieContext ctx) => context = ctx;
         public IActionResult Index()
         {
-            return View();
+            var movies = context.Movies.OrderBy(m => m.Name).ToList();
+            return View(movies);
         }
     }
 }
